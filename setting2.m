0899b2d3c4a576c6da52a9aa7a9a34d10054673c
Yoshizumi-Ryo <138884581+Yoshizumi-Ryo@users.noreply.github.com>
2023-10-07 08:56:41 +0900

    Add files via upload

--- setting2.m
+++ setting2.m
@@ -1,10 +1,11 @@
 /*-----------------------------
 you compile the following order.
 
-1st.setting.m
+1st.setting2.m
 2nd.Mum_to_theta7.m
-3rd.additions6.m
-4th.isogeny.m
+3rd.additions7.m
+4th.isogeny3.m
+5th.test_setting2.m
 -----------------------------*/
 
 //=============================================
